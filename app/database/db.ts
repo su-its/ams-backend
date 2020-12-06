dff3372d40ce690f99a128cf1cc950ba3b843553
h-takeyeah <who26second@gmail.com>
2020-12-07 02:48:30 +0900

    Configure default value for db connection.

--- app/database/db.ts
+++ app/database/db.ts
@@ -3,11 +3,11 @@ require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env')}
 import { createConnection } from 'mysql2'
 
 const connection = createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306', 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB,
+  database: process.env.DB || 'accessdb',
   waitForConnections: true
 }).promise()
 
