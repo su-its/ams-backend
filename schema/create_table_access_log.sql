b15c4d8948db6e2f540d4d603296ce9c47309848
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2020-12-07 22:41:03 +0900

    Update create_table_access_log.sql

--- schema/create_table_access_log.sql
+++ schema/create_table_access_log.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `access_log`(
-  `member_id` NOT NULL,
+  `member_id` int unsigned NOT NULL,
   `entered_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `exited_at` datetime DEFAULT NULL,
   PRIMARY KEY (`member_id`, `entered_at`)
