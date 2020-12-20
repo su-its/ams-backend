373a5f661bb9ed47745ea26438bf8068ede7fe90
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2020-12-21 02:09:19 +0900

    Remove specific database name from db.ts. Rename column.

--- app/database/db.ts
+++ app/database/db.ts
@@ -1,13 +1,15 @@
 // user environment variable
-require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env')})
+import { config } from 'dotenv'
 import { createConnection } from 'mysql2'
+
+config()
 
 const connection = createConnection({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306', 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB || 'accessdb',
+  database: process.env.DB,
   waitForConnections: true
 }).promise()
 
--- schema/create_table_access_log.sql
+++ schema/create_table_access_log.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `access_log`(
-  `member_id` int unsigned NOT NULL,
+  `student_id` int unsigned NOT NULL,
   `entered_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `exited_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`member_id`, `entered_at`)
+  PRIMARY KEY (`student_id`, `entered_at`)
 )
