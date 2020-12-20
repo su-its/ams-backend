007fc3a1a0ae511e4b7239899b0306ed9f96dd9f
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2020-12-21 02:29:07 +0900

    Create out-slack.ts (Include the factory like class)

---
+++ app/slack/out-slack.ts
@@ -0,0 +1,74 @@
+import https from 'https'
+
+export class SlackPostMessageFactory {
+  private channel = ''
+  private footer = ''
+  private text = ''
+  private token: string
+
+  constructor(token: string | undefined) {
+    if (typeof token === 'undefined') throw new Error('Slack bearer token is undefined!')
+    this.token = token as string
+  }
+
+  setChannel(channelSendTo: string) {
+    this.channel = channelSendTo
+  }
+
+  setFooter(s: string, ...rest: string[]) {
+    /* 'text' must be more than 0 characters */
+    if (s.length === 0) s = '_test_'
+    rest.unshift(s)
+    this.footer = rest.join(' ')
+  }
+
+  setText(s: string, ...rest: string[]) {
+    /* 'text' must be more than 0 characters */
+    if (s.length === 0) s = '_test_'
+    rest.unshift(s)
+    this.text = rest.join(' ')
+  }
+
+  postMessage() {
+    const payload = {
+      channel: this.channel,
+      text: 'from boushitsu',
+      icon_url: 'https://avatars.slack-edge.com/2017-04-21/172510263652_25951d12c20f18b77184_88.png',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: this.text
+          }
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: this.footer
+            }
+          ]
+        }
+      ]
+    }
+
+    const req = https.request({
+      hostname: 'slack.com',
+      path: '/api/chat.postMessage',
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      }
+    })
+
+    req.on('error', err => console.error(err))
+    req.on('close', () => console.log('CLOSE'))
+    req.on('finish', () => console.log('FINISH'))
+    req.on('response', res => console.log('STATUS', res.statusCode))
+    req.write(JSON.stringify(payload))
+    req.end()
+  }
+}
