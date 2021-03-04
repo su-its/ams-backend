247a11264bbedcda0d56a2734fc9517a21d7e8d9
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-04 12:50:01 +0900

    Impliment functions to play sound. Add sound/

--- app/controllers/readerInputController.ts
+++ app/controllers/readerInputController.ts
@@ -1,8 +1,11 @@
-import { spawn } from 'child_process'
+import { spawnSync } from 'child_process'
 import { Request, Response } from 'express'
+import { join } from 'path'
 import * as roomTable from '../models/inRoomUsersModel'
 import * as logsTable from '../models/accessLogsModel'
 import mysql from '../database/db'
+
+const WAV_FILE_DIR = join(process.cwd(), 'sound')
 
 const Status = {
   SUCCESS: 'success',
@@ -10,32 +13,51 @@ const Status = {
   FATAL: 'fatal'
 } as const
 
+function playWav (...fileName: string[]) {
+  fileName.forEach(f => {
+    // stdio: 'inherit'でnodeのstdioを子プロセスにも使わせる
+    spawnSync('aplay', ['-q', f + '.wav'], { cwd: WAV_FILE_DIR, stdio: 'inherit' })
+  })
+}
+
 const handleReaderInput = async (req: Request, res: Response) => {
-  res.status(200).send()
+  // reader-bridgeにOKを帰す
+  res.status(200).send('OK')
 
   const readerStatus = req.body.status
   const receivedUserId = req.body.user_id
 
-  if (!readerStatus || !receivedUserId) {
-    // spawn('aplay', ['-q', 'error.wav'], { cwd: process.cwd() })
+  const isValidStatus = (s: string) => {
+    for (const itr of Object.values(Status)) {
+      if (itr === s) return true
+    }
+    return false
+  }
+
+  // statusとしておかしかったら止める
+  if (!isValidStatus(readerStatus)) {
+    playWav(Status.FATAL)
     return
   }
 
+  // reader-bridgeからのリクエストを正しく受け取ったことを音で知らせる
   switch (readerStatus) {
     case Status.SUCCESS:
-      // spawn('aplay', ['-q', 'success.wav'], { cwd: process.cwd() })
+      playWav(Status.SUCCESS)
       break
     case Status.ERROR:
-      // spawn('aplay', ['-q', 'error.wav'], { cwd: process.cwd() })
-      break
+      playWav(Status.ERROR)
+      return
     case Status.FATAL:
-      // spawn('aplay', ['-q', 'fatal.wav'], { cwd: process.cwd() })
-      break
+      playWav(Status.FATAL)
+      return
     default:
-      /* never */
-      // spawn('aplay', ['-q', 'error.wav'], { cwd: process.cwd() })
+      /* isValidStatus()でちぇっくするので多分ここには来ない */
+      playWav(Status.FATAL)
+      return
   }
 
+  // 入室or退室処理
   const [user, error] = await roomTable.getUser(receivedUserId)
   if (error) {
     // 例えばconsole.log(error)などしてログに残したい
@@ -45,13 +67,18 @@ const handleReaderInput = async (req: Request, res: Response) => {
   try {
     await mysql.beginTransaction()
     if (user) {
+      // 退室
+      playWav('out')
       await logsTable.createAccessLog(user.user_id, user.entered_at)
       await roomTable.deleteUser(user.user_id)
     } else {
+      // 入室
+      playWav('in')
       await roomTable.createUser(receivedUserId)
     }
     await mysql.commit()
   } catch (error) {
+    console.error(error)
     await mysql.rollback()
   }
 }
