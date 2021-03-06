3a46a0bdd84025f2f0f424518a81efe7490453e4
Satake <satake00157@gmail.com>
2021-03-07 01:47:35 +0900

    シングルクォーテーションを削除。 tableの定義にutf8mb4と、InnoDBをセット

--- schema/create_table_access_logs.sql
+++ schema/create_table_access_logs.sql
@@ -1,8 +1,6 @@
-USE `Entering_And_Leaving_The_Room`;
-
-CREATE TABLE IF NOT EXISTS `access_logs`(
-  `user_id` int unsigned NOT NULL,
-  `entered_at` datetime NOT NULL,
-  `exited_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`, `entered_at`)
-)
+CREATE TABLE IF NOT EXISTS access_logs(
+  user_id int unsigned NOT NULL,
+  entered_at datetime NOT NULL,
+  exited_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, entered_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- schema/create_table_in_room_users.sql
+++ schema/create_table_in_room_users.sql
@@ -1,7 +1,5 @@
-USE `Entering_And_Leaving_The_Room`;
-
-CREATE TABLE IF NOT EXISTS `in_room_users` (
-  `user_id` int unsigned NOT NULL,
-  `entered_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
-)
+CREATE TABLE IF NOT EXISTS in_room_users (
+  user_id int unsigned NOT NULL,
+  entered_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
