7777140c85dce3660d7fa6b718c4f50051bc879d
Satake <satake00157@gmail.com>
2021-03-09 12:09:19 +0900

    workdirをマウントしてるフォルダにすることで、絶対パスを書く手間を省略 Redpenの出力結果を、Redpenフォルダに出すようにした。

--- docker/redpen/docker-entrypoint.sh
+++ docker/redpen/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+# 出力先ディレクトリの作成
+mkdir Redpen
+
 # mdファイルをループ
-for file in /redpen/*.md ; do
+for file in *.md; do
     # *.mdというファイルが存在した場合の対処
     [ -f "$file" ] || continue
-
-    # Redpenを使用
-    redpen -c /redpen/docs/config/Redpen-config.xml -f markdown -r plain "$file"
-    echo "$file"
+    redpen -t info -c /redpen/docs/config/Redpen-config.xml -f markdown -r plain2 "$file">>Redpen/Result.txt
 done
