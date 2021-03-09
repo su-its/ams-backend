69bf41c618055a8d103c84c77ef6dd619585d4a6
Satake <satake00157@gmail.com>
2021-03-09 22:20:42 +0900

    再帰的にmdのチェックを行えるようになった。

--- docker/redpen/docker-entrypoint.sh
+++ docker/redpen/docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 # 出力先ディレクトリの作成
-mkdir Redpen
+mkdir -p Redpen
 
-# mdファイルをループ
-for file in *.md; do
-    # *.mdというファイルが存在した場合の対処
-    [ -f "$file" ] || continue
-    redpen -t info -c /redpen/docs/config/Redpen-config.xml -f markdown -r plain2 "$file">>Redpen/Result.txt
+# 再帰的にmdファイルを探す(node_modules以下のディレクトリは無視)
+search_data=$(find . \( -type d -and -name 'node_modules' -and -prune \) -or \( -name "*.md" \))
+
+# mdファイルをループ(残ってしまった./node_modulesはスキップするようにした)
+for file in $search_data; do
+    if [ $file = './node_modules' ]; then
+        continue
+    fi
+    redpen -t info -c ./docs/config/Redpen-config.xml -f markdown -r plain2 $file >>Redpen/Result.txt
 done
