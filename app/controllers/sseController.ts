9b24d914eb1044e1f055115c8b7abd3988009380
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-10 22:05:03 +0900

    SSEが動かなかったのでヘッダーの設定方法を修正

--- app/controllers/sseController.ts
+++ app/controllers/sseController.ts
@@ -2,9 +2,17 @@ import { Request, Response } from 'express'
 import { emitter } from './readerInputController'
 import * as roomTable from '../models/inRoomUsersModel'
 
+/**
+ * CORS用
+ */
+const frontEndOrigin = 'http://localhost:8000'
+
 export function sseHandler (req: Request, res: Response) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
-  res.setHeader('Content-Type', 'text/event-stream')
+  res.set({
+    'Access-Control-Allow-Origin': frontEndOrigin,
+    'Content-Type': 'text/event-stream' // event-streamでクライアント-サーバー間を繋ぎっぱなしにする
+  })
+  res.status(200).write('data: hello\n\n') // nnは必須
 
   emitter.on('usersUpdated', async () => {
     const [users, err] = await roomTable.listUsers()
@@ -12,8 +20,7 @@ export function sseHandler (req: Request, res: Response) {
       // do something
       res.status(500).send('error')
     } else {
-      // do something
-      res.status(200).json(users)
+      res.status(200).write(`data: ${JSON.stringify(users)}\n\n`)
     }
   })
 }
