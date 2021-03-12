7b7b98443171980b54b89ca771950e6e95ae476f
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-13 03:54:54 +0900

    クライアントとの接続を配列に保持しておくように変更

--- app/controllers/sseController.ts
+++ app/controllers/sseController.ts
@@ -2,7 +2,6 @@
 import { Request, Response } from 'express'
 import { emitter } from './readerInputController'
 import * as roomTable from '../models/inRoomUsersModel'
-import EventEmitter from 'events'
 
 // TODO あとでmodelsかどこかのファイルに移動する
 /**
@@ -21,16 +20,54 @@ interface NamedInRoomUser extends InRoomUser {
   'user_name': string | null
 }
 
+const clients: Response[] = []
+
 /**
- * @param {Express.Request} _req - HTTPリクエスト
- * @param {Express.Response} res - HTTPレスポンス
- * `/{version}/sse`に来たリクエストにストリームを開いて繋ぎっぱなしにする。
- * 在室者のリストの変更を検知して更新されたリストをpush(≒プッシュ通知)する。
+ * `usersUpdated`という`event`フィールドを持つSSEを配列`clients`内の全ての
+ * クライアントに対して投げる関数。`emitter`(events.EventEmitter)の発火する
+ * `usersUpdated`イベントのコールバック関数として使用する。80行目あたりを参照。
  *
  * Server-Sent eventsについて
  * https://html.spec.whatwg.org/multipage/server-sent-events.html
  */
+async function sendUsersUpdatedEvent () {
+  const [users, err] = await roomTable.listUsers()
+  if (err) {
+    // listUsers()でエラーがあったら一旦全クライアントとのストリームを閉じる。
+    for (const res of clients) {
+      // status codeが4xxや5xxだった時点でクライアント側ではEventSourceのerrorイベントが
+      // 発火し、数秒後に再接続をトライしてくる。だからここは迷わずコネクションを切ってよし。
+      // ただし何度もリトライされても困るのでクライアントには3回トライしたら止めるなど配慮してほしい。
+      res.status(500).json({ message: err?.message || 'internal server error' })
+    }
+  } else {
+    for (const user of users as NamedInRoomUser[]) {
+      user.user_name = null
+    }
+
+    try {
+      const json = JSON.stringify(users)
+      // イベント名は任意。全て小文字の方がいいのかな?
+      for (const res of clients) {
+        res.status(200).write(`event: usersUpdated\ndata: ${json}\n\n`)
+      }
+    } catch (err) {
+      console.error(err)
+      for (const res of clients) {
+        res.status(500).json({ message: 'internal server error' })
+      }
+    }
+  }
+}
+
+/**
+ * @param {Express.Request} _req - HTTPリクエスト
+ * @param {Express.Response} res - HTTPレスポンス
+ * `/{version}/users_updated_event`に来たリクエストについて対応するレスポンスに
+ * 適切なレスポンスヘッダーを設定して、それを配列`clients`に格納する関数。
+ */
 function sseHandler (_req: Request, res: Response) {
+  // 適切なレスポンスヘッダーを設定
   res.set({
     'Content-Type': 'text/event-stream', // event-streamでクライアント-サーバー間を繋ぎっぱなしにする
     'Cache-Control': 'no-store' // レスポンスがキャッシュに保存されないようにする(クライアント側でも同じことをしているが一応)
@@ -39,39 +76,22 @@ function sseHandler (_req: Request, res: Response) {
   // hello world
   res.status(200).write('data: hello\n\n') // nnは必須 コネクションが切れてしまうのでsend()は使わない
 
-  // ブラウザーの再読み込みを繰り返すとその度にEventEmitterにlistenerが追加され、10を超えると
-  // 'MaxListenersExceededWarning: Possible EventEmitter memory leak detected.'
-  // のような警告が出る。とりあえずは警告が出そうになったらこのイベントに登録済みのlistenerたちを解除する
-  // TODO EventEmitterからlistenerが外れてもフロントエンド側とのコネクションが切れるわけではないのでそこを何とかする
-  if (emitter.listenerCount('usersUpdated') + 1 > EventEmitter.defaultMaxListeners) {
-    emitter.removeAllListeners('usersUpdated')
-  }
+  // リストに追加
+  clients.push(res)
+  // printCurState() // デバッグ用
+}
 
-  // readerInputControllerで処理が走るとuserUpdatedイベントが発火するので、
-  // そのタイミングでクライアントにpushするためにリスナーを登録する
-  emitter.on('usersUpdated', async function sendUsersUpdatedEvent () {
-    const [users, err] = await roomTable.listUsers()
-    if (err) {
-      // status codeが4xxや5xxだった時点でクライアント側ではEventSourceのerrorイベントが
-      // 発火する。たぶん数秒後に再接続をトライする
-      res.status(500).json({ message: err?.message || 'internal server error' })
-    } else {
-      // TODO
-      // とりあえずuser_nameは仮で割り当てる。user_nameはstringかnullにしてある(undefinedではない!)
-      // 最悪Array<any>でも可
-      (users as NamedInRoomUser[]).forEach(user => {
-        user.user_name = null
-      })
-      try {
-        const json = JSON.stringify(users)
-        // イベント名は任意。全て小文字の方がいいのかな?
-        res.status(200).write(`event: usersUpdated\ndata: ${json}\n\n`)
-      } catch (err) {
-        console.error(err)
-        res.status(500).json({ message: 'internal server error' })
-      }
-    }
-  })
+// ここが最初に実行される
+// listenerの登録
+emitter.on('usersUpdated', sendUsersUpdatedEvent)
+
+// eslint-disable-next-line no-unused-vars
+function printCurState () {
+  console.log(new Date().toISOString(), 'Current State')
+  for (const evtName of emitter.eventNames()) {
+    console.log(`${emitter.listenerCount(evtName)} listener(s) is listening to ${String(evtName)} event.`)
+  }
+  console.log(`Current clients has ${clients.length} connection(s).`)
 }
 
 export { sseHandler }
