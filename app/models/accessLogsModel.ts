0e9503e522d117b81e4266598195046a66238e67
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-17 00:06:43 +0900

    accessLogsModel.ts: 配列を返すのをやめた

--- app/models/accessLogsModel.ts
+++ app/models/accessLogsModel.ts
@@ -4,53 +4,46 @@ import mysql from '../database/db'
 const TABLENAME = 'access_logs'
 
 async function createAccessLog (userId: number, enteredAt: string) {
-  try {
-    // INSERT すると [ResultSetHeader, undefined] が返ってくる
-    await mysql.query(`INSERT INTO ${TABLENAME} (user_id, entered_at) VALUES (?, ?)`,
-      [userId, enteredAt])
-    return [0, null]
-  } catch (err) {
-    return [null, err]
-  }
+  // INSERT すると [ResultSetHeader, undefined] が返ってくる
+  await mysql.query(`INSERT INTO ${TABLENAME} (user_id, entered_at) VALUES (?, ?)`,
+    [userId, enteredAt])
 }
 
 /**
  * access_logsの総行数を取得する
  */
 async function getCountOfAccessLogs () {
-  try {
-    // https://stackoverflow.com/a/11412968
-    const [rows, _] = await mysql.query(`SELECT count(*) AS logCount FROM ${TABLENAME}`)
+  // https://stackoverflow.com/a/11412968
+  const [rows, _] = await mysql.query(`SELECT count(*) AS logCount FROM ${TABLENAME}`)
 
-    if (Array.isArray(rows) && rows.length === 1) {
-      const row = rows[0]
-      if ('logCount' in row) {
-        return [row.logCount, null]
-      }
+  if (Array.isArray(rows) && rows.length === 1) {
+    const row = rows[0]
+    if ('logCount' in row) {
+      // SQLの仕様からしてcount()の返り値は整数なのでアサーションしても問題ない
+      return row.logCount as number
     }
-
-    return [null, 'response from DB is malformed']
-  } catch (error) {
-    return [null, error]
   }
+
+  throw new Error('response from DB is malformed')
 }
 
 async function listAccessLogs (
   since?: string, until?: string,
   order = 'DESC', limit = 100, offset = 0
 ) {
-  try {
-    const [rows, _] = await mysql.query(
-      `SELECT * FROM ${TABLENAME} ` +
-      "WHERE exited_at >= IFNULL(?, '1970-01-01') " +
-      'AND entered_at < IFNULL(?, ADDDATE(CURDATE(), 1)) ' +
-      `ORDER BY entered_at ${['ASC', 'DESC'].includes(order) ? order : 'DESC'} ` +
-      'LIMIT ? OFFSET ?',
-      [since, until, limit, offset])
-    return [rows, null]
-  } catch (err) {
-    return [null, err]
+  const [rows, _] = await mysql.query(
+    `SELECT * FROM ${TABLENAME} ` +
+    "WHERE exited_at >= IFNULL(?, '1970-01-01') " +
+    'AND entered_at < IFNULL(?, ADDDATE(CURDATE(), 1)) ' +
+    `ORDER BY entered_at ${['ASC', 'DESC'].includes(order) ? order : 'DESC'} ` +
+    'LIMIT ? OFFSET ?',
+    [since, until, limit, offset])
+
+  if (Array.isArray(rows)) {
+    return rows
   }
+
+  throw new Error('response from DB is malformed')
 }
 
 export { createAccessLog, getCountOfAccessLogs, listAccessLogs }
