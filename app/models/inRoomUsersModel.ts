08d77acef9fac33ec63bf8767a23d8de2f486360
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-17 12:29:45 +0900

    inRoomUsersModel.ts: 配列を返すのをやめた

--- app/models/inRoomUsersModel.ts
+++ app/models/inRoomUsersModel.ts
@@ -4,45 +4,41 @@ import mysql from '../database/db'
 const TABLENAME = 'in_room_users'
 
 async function createUser (userId: number) {
-  try {
-    // INSERT すると [ResultSetHeader, undefined] が返ってくる
-    await mysql.query(`INSERT INTO ${TABLENAME} (user_id) VALUES (?)`, userId)
-    return [0, null]
-  } catch (err) {
-    return [null, err]
-  }
+  // INSERT すると [ResultSetHeader, undefined] が返ってくる
+  await mysql.query(`INSERT INTO ${TABLENAME} (user_id) VALUES (?)`, userId)
 }
 
 async function deleteUser (userId: number) {
-  try {
-    // DELETE すると [ResultSetHeader, undefined] が返ってくる
-    await mysql.query(`DELETE FROM ${TABLENAME} WHERE user_id=?`, userId)
-    return [0, null]
-  } catch (err) {
-    return [null, err]
-  }
+  // DELETE すると [ResultSetHeader, undefined] が返ってくる
+  await mysql.query(`DELETE FROM ${TABLENAME} WHERE user_id=?`, userId)
 }
 
 async function getUser (userId: number) {
-  try {
-    const [row, _] = await mysql.query(`SELECT * FROM ${TABLENAME} WHERE user_id=?`, userId)
-    if (Array.isArray(row) && row.length !== 0) {
-      return [row[0], null]
-    } else {
-      return [null, null]
+  const [row, _] = await mysql.query(`SELECT * FROM ${TABLENAME} WHERE user_id=?`, userId)
+  if (Array.isArray(row) && row.length <= 1) {
+    if (row.length === 0) {
+      return null
+    // Array.isArray(row[0])はquery()が二次元配列を返すパターンがある。
+    // このクエリ内容ならそれはあり得ないのだが念のため。
+    // あり得ないので型アサーションで型を教えても問題ないといえばない。
+    } else if (Array.isArray(row[0])) {
+      throw new Error('response from DB is invalid')
+    // user_idはこのテーブルの主キーなので0でないとすれば必ず1なのだが一応。
+    } else if (row.length === 1) {
+      return row[0]
     }
-  } catch (err) {
-    return [null, err]
   }
+
+  throw new Error('response from DB is malformed')
 }
 
 async function listUsers () {
-  try {
-    const [row, _] = await mysql.query(`SELECT * FROM ${TABLENAME}`)
-    return [row, null]
-  } catch (err) {
-    return [null, err]
+  const [rows, _] = await mysql.query(`SELECT * FROM ${TABLENAME}`)
+  if (Array.isArray(rows)) {
+    return rows as []
   }
+
+  throw new Error('response from DB is not an array')
 }
 
 export { createUser, deleteUser, getUser, listUsers }
