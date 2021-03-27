56578748845e69bad65db3c82083b728c352dc42
equal-l2 <eng.equall2@gmail.com>
2021-03-27 21:23:16 +0900

    アクセスログを ./logs/access.log に書き込むようにした

--- ams-backend.ts
+++ ams-backend.ts
@@ -1,7 +1,9 @@
 import cors from 'cors'
-import express from 'express'
-import morgan from 'morgan'
 import dayjs from 'dayjs'
+import express from 'express'
+import fs from 'fs'
+import morgan from 'morgan'
+import path from 'path'
 import 'dayjs/locale/ja'
 import * as accesslogsRoutes from './app/routes/accessLogsRoutes'
 import * as inRoomUsersRoutes from './app/routes/inRoomUsersRoutes'
@@ -9,7 +11,33 @@ import * as readerInputRoutes from './app/routes/readerInputRoutes'
 import * as sseRoutes from './app/routes/sseRoutes'
 import { amsOptions } from './config'
 
+function prepareMorgan () {
+  // morganの:dateはUTCなので、日本時間を表示するものを作る
+  morgan.token('localdate', () => dayjs().locale('ja').format())
+
+  // ログを吐くフォルダを作る
+  const logFolder = path.join(__dirname, '/logs')
+  fs.mkdirSync(logFolder)
+
+  // ログファイルのストリームを開く
+  const wStream = fs.createWriteStream(
+    path.join(logFolder, '/access.log'),
+    {
+      flags: 'a'
+    }
+  )
+
+  const formatStr = ':remote-addr - :remote-user [:localdate] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
+
+  return morgan(formatStr, {
+    stream: wStream
+  })
+}
+
 const app: express.Express = express()
+
+// logging
+app.use(prepareMorgan())
 
 // parse requests of content-type - application/json
 app.use(express.json())
@@ -19,12 +47,6 @@ app.use(express.urlencoded({ extended: true }))
 
 // CORS
 app.use(cors())
-
-// logging
-app.use(morgan(':remote-addr - :remote-user [:localdate] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'))
-
-// morganの:dateはUTCなので、日本時間のものを作る
-morgan.token('localdate', () => dayjs().locale('ja').format())
 
 // simple route
 app.get('/', (_req, res) => {
